6cc828c81b68f0bc3d5b39c28952753207cb37d6
rosemariestoica13 <rose.stoica@student.upt.ro>
2026-07-06 14:04:51 +0300

    Suppress the harmless xFormers-not-available log warning
    Raise the 'dinov2' logger level before importing the DepthAnythingV2
    backbone, since we intentionally don't install xformers (optional
    speed-up, not needed for this demo).

--- model/__init__.py
+++ model/__init__.py
@@ -1,3 +1,9 @@
+import logging
+
+# thirdparty/DepthAnythingV2's dinov2_layers log a "xFormers not available"
+# warning at import time; xformers is an optional speed-up we don't install.
+logging.getLogger("dinov2").setLevel(logging.ERROR)
+
 from model.build_match_flow import BuildMatchFlow
 
 
